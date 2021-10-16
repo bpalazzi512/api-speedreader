a141cc4938c87e529fa67a82d04d6a5e5266b913
Donsou <donsoul78@gmail.com>
2021-10-16 09:15:25 -0400

    skeleton

--- window.py
+++ window.py
@@ -2,10 +2,20 @@ from tkinter import *
 from tkinter.ttk import *
 import tkinter as tk
 from main import callAgain
+import time
 
 window = tk.Tk()
 label = tk.Label(text="Your Texts:")
-labelTwo = tk.Label(text = callAgain())
+textFromMain = callAgain()
+labelTwo = tk.Label(text = textFromMain)
+textBox = tk.Label(text = " ")
+button = tk.Button(
+    text="Click me!",
+    width=25,
+    height=5,
+    bg="blue",
+    fg="yellow",
+)
 
 widgetLabel = tk.Label(text="WPM")
 entry = tk.Entry()
@@ -14,5 +24,34 @@ label.pack()
 labelTwo.pack()
 widgetLabel.pack()
 entry.pack()
+textBox.pack()
 
+
+
+def telempromter():
+    words = len(textFromMain)
+    wpm = int(entry.get())
+    #list = textFromMain.split()
+    for i in range (words):
+        list = textFromMain.split()[i]
+        textBox = tk.Label(text = list)
+        textBox.pack()
+        print(list)
+        window.update()
+    
+        time.sleep(1/wpm)
+
+def startProgram(event):
+    for i in range (3):
+        textBox = tk.Label(text = i+1)
+        textBox.pack()
+        window.update()
+        time.sleep(1)
+    
+    telempromter()
+
+
+window.bind("<Return>", startProgram)
 window.mainloop()
+
+
